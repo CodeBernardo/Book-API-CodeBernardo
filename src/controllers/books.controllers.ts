884d388d789467c5f32ef997f3ee28c463d0c9b8
CodeBernardo <stein.bernardo@proton.me>
2023-12-13 15:06:47 -0300

    feat: book controllers

--- src/controllers/books.controllers.ts
+++ src/controllers/books.controllers.ts
@@ -1,5 +1,33 @@
 import { Request, Response } from "express";
+import { BookServices } from "../services/books.services";
+import { booksDatabase } from "../database/database";
 
 class BookControllers {
-  static createBook = (req: Request)
+  static createBook = (req: Request, res: Response): Response => {
+    const newBook = BookServices.createBook(req.body);
+    return res.status(201).json(newBook);
+  };
+
+  static getBooks = (req: Request, res: Response): Response => {
+    const booksList = BookServices.getBooks();
+    return res.status(200).json(booksList);
+  };
+
+  static retrieveBook = (req: Request, res: Response): Response => {
+    const bookFound = BookServices.retrieveBook(res.locals.bookIndex);
+    return res.status(200).json(bookFound);
+  };
+
+  static updateBook = (req: Request, res: Response): Response => {
+    const bookIndex = res.locals.bookIndex;
+    BookServices.updateBook(bookIndex, req.body);
+    return res.status(202).json(booksDatabase[bookIndex]);
+  };
+
+  static deleteBook = (req: Request, res: Response): void => {
+    const bookIndex = res.locals.bookIndex;
+    BookServices.deleteBook(bookIndex);
+  };
 }
+
+export { BookControllers };
